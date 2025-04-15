2e35d34b124dc1fea09c5d84186ef5d89e182515
DANTE-KUN3 <parikhbishal@gmail.com>
2025-04-15 20:07:42 +0530

    new commit

--- terraform/module/ec2/main.tf
+++ terraform/module/ec2/main.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "new" {
   instance_type          = var.instance_type
   key_name               = var.key_name
   vpc_security_group_ids = var.security_group_ids
-#new
+
   user_data = <<-EOF
               #!/bin/bash
               sudo yum update -y
