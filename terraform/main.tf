d78627829894d84909bfdc9342e395dc43ce5866
DANTE-KUN3 <parikhbishal@gmail.com>
2025-04-15 18:13:35 +0530

    added var value

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,15 @@
+
+terraform {
+  backend "s3" {
+    bucket         = "bee2025"
+    key            = "terraform.tfstate"
+    region         = "us-west-2"
+    encrypt        = true
+    use_lockfile = true
+
+  }
+}
+
 provider "aws" {
   region = "us-east-1"
 }
