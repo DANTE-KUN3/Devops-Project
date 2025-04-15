f98be4001c7a3afcb56acc0642af31bf680ae1fe
DANTE-KUN3 <parikhbishal@gmail.com>
2025-04-15 19:11:25 +0530

    added var value

--- terraform/module/ec2/main.tf
+++ terraform/module/ec2/main.tf
@@ -4,12 +4,25 @@ resource "aws_instance" "new" {
   key_name               = var.key_name
   vpc_security_group_ids = var.security_group_ids
 
-   user_data = <<-EOF
+    user_data = <<-EOF
               #!/bin/bash
               # Update packages
               apt-get update -y
-              # Install Python and pip
-              apt-get install -y python3 python3-pip
+              # Install dependencies to add Python repository
+              apt-get install -y software-properties-common
+
+              # Add the deadsnakes PPA which contains newer Python versions
+              add-apt-repository ppa:deadsnakes/ppa
+              apt-get update -y
+
+              # Install Python 3.8 or later
+              apt-get install -y python3.8 python3.8-distutils python3.8-venv
+
+              # Ensure pip for Python 3.8 is installed
+              apt-get install -y python3-pip
+
+              # Verify the Python version
+              python3.8 --version
               EOF
 
   tags = {
