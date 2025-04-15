a6e7fd7797d453c29a2f8d55c377a86825e117c7
DANTE-KUN3 <parikhbishal@gmail.com>
2025-04-15 21:18:38 +0530

    added var value

--- terraform/module/ec2/main.tf
+++ terraform/module/ec2/main.tf
@@ -12,26 +12,23 @@ resource "aws_instance" "new" {
               # Install dependencies to compile Python from source
               sudo yum install gcc openssl-devel bzip2-devel libffi-devel zlib-devel -y
 
-              # Install Python 3.8 using yum (Amazon Linux 2 supports Python 3.8)
+              # Install Python 3.8
               sudo yum install python3.8 -y
 
-              # Make sure to update alternatives to set python3.8 as the default Python
-              sudo alternatives --install /usr/bin/python python /usr/bin/python3.8 1
-              sudo alternatives --set python /usr/bin/python3.8
+              # Create a symbolic link to make Python 3 the default
+              sudo ln -sf /usr/bin/python3.8 /usr/bin/python
+
+              # Verify Python installation
+              python --version
 
               # Install pip for Python 3.8
               sudo curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
               sudo python get-pip.py
 
-              # Verify Python and pip installations
-              python --version
+              # Verify pip installation
               pip --version
 
-              # Ensure the system uses the correct Python version
-              sudo ln -sf /usr/bin/python3.8 /usr/bin/python
-              sudo ln -sf /usr/bin/pip3 /usr/bin/pip
-
-              # Restart the machine (optional, for good measure)
+              # Reboot to ensure changes take effect
               sudo reboot
               EOF
 
